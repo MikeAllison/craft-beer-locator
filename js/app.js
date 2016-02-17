91615523308fbcb1c096554b2aed6b3753a5d704
Mike Allison <mikeallison79@gmail.com>
2016-02-17 16:42:29 -0500

    AJAX Request working.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 (function() {
+
   var httpRequest;
+
   document.getElementById("submitButton").onclick = function() {
     var cityState = document.getElementById("cityState").value;
     getLocation(cityState);
@@ -7,27 +9,32 @@
 
   function getLocation(cityState) {
     httpRequest = new XMLHttpRequest();
+    var apiKey = 'AIzaSyBCaX60okxecYLD05GC745IP1u6nzwKDSo';
+    var params = '?' + apiKey + '&address=' + encodeURIComponent(cityState);
+    var url = 'https://maps.googleapis.com/maps/api/geocode/json' + params;
 
     if (!httpRequest) {
       alert('Sorry, your request could not be completed.');
       return false;
     }
 
-    httpRequest.onReadyStateChange = alertContents;
-    httpRequest.open('POST', 'https://maps.googleapis.com/maps/api/geocode/json', true);
+    httpRequest.onreadystatechange = alertContents;
+    httpRequest.open('GET', url, true);
     httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    var params = '?address=Boston,MA&key=AIzaSyBCaX60okxecYLD05GC745IP1u6nzwKDSo';
-    console.log(params);
-    httpRequest.send(params);
+    httpRequest.send();
   }
 
   function alertContents() {
     if (httpRequest.readyState === XMLHttpRequest.DONE) {
       if (httpRequest.status === 200) {
-        alert('worked');
+        var response = JSON.parse(httpRequest.responseText);
+        var lat = response.results[0].geometry.location.lat
+        var lng = response.results[0].geometry.location.lng
+        console.log(lat + ' ' + lng);
       } else {
         alert('Sorry, please try again.');
       }
     }
   }
+
 })();
