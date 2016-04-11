cd747aff8c477c326d3b9f46d1911b8b2b4680d2
Mike Allison <mikeallison79@gmail.com>
2016-04-11 11:20:26 -0400

    Added geolocation code.  Added view code for alerts.

--- js/app.js
+++ js/app.js
@@ -3,27 +3,63 @@
 
   model = {
     init: function() {
-      this.currentLoc = { lat: null, lng: null };
+      this.location = {};
+    },
+    setLatLng: function(lat, lng) {
+      this.location = { lat: lat, lng: lng };
     }
   };
 
   controller = {
     init: function() {
+      this.apiKey = 'AIzaSyBCaX60okxecYLD05GC745IP1u6nzwKDSo';
       model.init();
       views.form.init();
+      views.alerts.init();
     },
     getCurrentLocation: function() {
       navigator.geolocation.getCurrentPosition(function(position) {
-        // Store this position
-        console.log(position);
+        model.setLatLng(position.coords.latitude, position.coords.longitude);
       });
+    },
+    getGeocode: function() {
+      var tboxVal = views.form.cityStateTbox.value;
+      var url = 'https://maps.googleapis.com/maps/api/geocode/json?';
+      var params = 'key=' + controller.apiKey + '&address=' + encodeURIComponent(tboxVal);
+
+      var httpRequest = new XMLHttpRequest();
+      if (!httpRequest) {
+        views.alerts.tryAgain();
+        return false;
+      }
+
+      httpRequest.onload = function() {
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+          if (httpRequest.status !== 200) {
+            views.alerts.tryAgain();
+            return;
+          } else {
+            var response = JSON.parse(httpRequest.responseText);
+
+            if (response.status === 'ZERO_RESULTS' || response.results[0].geometry.bounds === undefined) {
+              views.alerts.notFound();
+              return;
+            } else {
+              model.setLatLng(response.results[0].geometry.location.lat, response.results[0].geometry.location.lng);
+            }
+          }
+        }
+      };
+
+      httpRequest.open('GET', url + params, true);
+      httpRequest.send();
     }
   };
 
   views = {
     form: {
       init: function() {
-        // Store DOM elements
+        // Collect DOM elements
         this.cityStateTbox = document.getElementById('cityStateTbox');
         this.searchBtn = document.getElementById('searchBtn');
         this.geoLocationBtn = document.getElementById('geoLocationBtn');
@@ -33,9 +69,30 @@
         this.cityStateTbox.setAttribute('placeholder', 'New York, NY');
         this.moreResultsBtn.classList.add('hidden');
         // Add click handlers
+        this.searchBtn.addEventListener('click', function() {
+          controller.getGeocode();
+        });
         this.geoLocationBtn.addEventListener('click', function(){
           controller.getCurrentLocation();
         });
+      }
+    },
+    alerts: {
+      init: function() {
+        // Collect DOM elements
+        this.alertDiv = document.getElementById('alertDiv');
+        // Set default values on DOM elements
+        this.alertDiv.classList.add('hidden');
+      },
+      tryAgain: function() {
+        this.alertDiv.textContent = 'Sorry, please try again.';
+        this.alertDiv.classList.add('alert-danger');
+        this.alertDiv.classList.remove('hidden');
+      },
+      notFound: function() {
+        this.alertDiv.textContent = 'Sorry, that location could not be found.';
+        this.alertDiv.classList.add('alert-danger');
+        this.alertDiv.classList.remove('hidden');
       }
     },
     results: {
