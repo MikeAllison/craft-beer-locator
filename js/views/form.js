fd935c33ddb41023a5a5cdb7ece2640ee3702039
Mike Allison <mikeallison79@gmail.com>
2016-05-05 12:17:36 -0400

    Grouped similar view code together to decrease the number of .js files

--- js/views/form.js
+++ js/views/form.js
@@ -1,3 +1,5 @@
+// Code related to the form and searching with the app
+
 var app = app || {};
 
 (function() {
@@ -39,6 +41,60 @@ var app = app || {};
     },
     enableSearchBtn: function() {
       this.searchBtn.removeAttribute('disabled');
+    }
+  };
+
+  // Location button
+  app.views.locationBtn = {
+    init: function() {
+      // Collect DOM elements
+      this.locationBtn = document.getElementById('locationBtn');
+      // Add click handlers
+      this.locationBtn.addEventListener('click', function() {
+        app.views.page.disableButtons();
+        app.views.page.clear();
+        app.controllers.geolocationSearch();
+      });
+    },
+    disable: function() {
+      this.locationBtn.setAttribute('disabled', true);
+    },
+    enable: function() {
+      this.locationBtn.removeAttribute('disabled');
+    }
+  };
+
+  // More results button if > 20 results are returned from the search
+  app.views.moreResultsBtn = {
+    init: function() {
+      // Collect DOM elements
+      this.moreResultsBtn = document.getElementById('moreResultsBtn');
+      // Set default values on DOM elements
+      this.moreResultsBtn.classList.add('hidden');
+      // Add click handlers
+      this.moreResultsBtn.addEventListener('click', function() {
+        app.views.page.disableButtons();
+        app.views.page.clear();
+        window.scroll(0, 0);
+      });
+    },
+    addNextPageFn: function() {
+      this.moreResultsBtn.onclick = function() {
+        app.controllers.requestMoreResults();
+      };
+    },
+    show: function() {
+      this.moreResultsBtn.classList.remove('hidden');
+      // Google Places search requires 2 seconds between searches
+      window.setTimeout(function() {
+        moreResultsBtn.removeAttribute('disabled');
+      }, 2000);
+    },
+    disable: function() {
+      this.moreResultsBtn.setAttribute('disabled', true);
+    },
+    hide: function() {
+      this.moreResultsBtn.classList.add('hidden');
     }
   };
 
--- js/views/page.js
+++ js/views/page.js
@@ -4,6 +4,14 @@ var app = app || {};
 
   app.views = app.views || {};
 
+  // Google Map's map object (needed for API but not displayed on page)
+  app.views.map = {
+    init: function() {
+      // Collect DOM elements
+      this.map = document.getElementById('map');
+    }
+  };
+
   // Control multiple items on the page
   app.views.page = {
     init: function() {
